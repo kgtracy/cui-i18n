777e3fca9d7b7b22265a2fd03fd2353c2aada25a
Ricardo Marques <ricardo.marques@rocklobstah.local>
2015-10-01 13:30:08 -0500

    gets all keys from locale-en_US

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
 (function(angular){
-	'use strict';
+    'use strict';
 
-	angular
-	.module('app',['translate'])
-	.controller('appCtrl',['$timeout',appCtrl]);
+    angular
+    .module('app',['translate'])
+    .factory('getVariables',['$http','$q',getVariables])
+    .controller('appCtrl',['$timeout','getVariables',appCtrl]);
+    
+    function getVariables($http){
+        return $http.get('dist/cui-i18n/angular-translate/locale-en_US.json')
+    }
 
-	function appCtrl($timeout){
-		var app=this;
-		app.init = function(){
-			app.test='test';
-			app.ammount=0;
-			app.increase();
-			app.date=new Date();
-			app.tick();
-		}
+    function appCtrl($timeout,getVariables){
+        var app=this;
+        app.init = function(){
+            app.key=[];
+            app.getVariables();
+            app.ammount=0;
+            app.increase();
+            app.date=new Date();
+            app.tick();
+        }
 
-		app.increase = function(){
-			if(app.ammount<1000000){
-				app.ammount+=0.01;
-				$timeout(app.increase,1);
-			}
-		}
+        app.getVariables = function(){
+            getVariables.then(function(res){
+                var i=0;
+                for(var key in res.data){
+                    app.key[i]=key;
+                    i++;
+                }
+            })
+        }
 
-		app.tick = function(){
-			app.date=new Date();
-			$timeout(app.tick,1000);
-		}
+        app.increase = function(){
+            if(app.ammount<1000000){
+                app.ammount+=0.01;
+                $timeout(app.increase,1);
+            }
+        }
 
-		app.init();
+        app.tick = function(){
+            app.date=new Date();
+            $timeout(app.tick,1000);
+        }
 
-	}
+        app.init();
+
+    }
+
+
 
 
 })(angular)
