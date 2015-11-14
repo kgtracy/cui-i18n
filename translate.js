1482a21f81f2dd8b2a143ef4003e786d712706b8
Ricardo Marques <ricardoalmmarques@gmail.com>
2015-11-13 18:37:10 -0600

    removes unwanted console.log and a bunch of comments.

--- translate.js
+++ translate.js
@@ -8,30 +8,13 @@
 		'pascalprecht.translate',// angular-translate
 		'tmh.dynamicLocale'// angular-dynamic-locale
 		])
-	// .constant('LOCALES', {
-	//     'locales': {
-	//     	//add more locales here
-	//         'pt_PT': 'Português (Portugal)',
-	//         'en_US': 'English (United States)',
-	//         'zh_CN': 'Chinese (China)',
-	//         'pl_PL': 'Polish (Poland)'
-	//     },
-	//     'preferredLocale': 'en_US'
-	// })
 	.config(['$translateProvider','tmhDynamicLocaleProvider', function ($translateProvider,tmhDynamicLocaleProvider) {
-		//get warning in console regarding forgotten ID's. Comment out when not needed.
+            //get warning in console regarding forgotten ID's. Comment out when not needed.
 	    $translateProvider.useMissingTranslationHandlerLog();
-
-	    //Asynchronously load translations
-	    // $translateProvider.useStaticFilesLoader({
-	    //     prefix: 'bower_components/cui-i18n/dist/cui-i18n/angular-translate/locale-',// path to translations files
-	    //     suffix: '.json'// suffix, currently- extension of the translations
-	    // });
 
 	    $translateProvider.preferredLanguage('en_US');// is applied on first load
 	    $translateProvider.useSanitizeValueStrategy('escape');
 	 
-
 	    //Where the $locale settings are (for currency,dates and number formats)
 	    tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
 
@@ -55,32 +38,18 @@
 	}])
 	.service('LocaleService', ['$translate', '$rootScope', 'tmhDynamicLocale',
 	 function ($translate,  $rootScope, tmhDynamicLocale) {
-	    // PREPARING LOCALES INFO
-	    var localesObj = {};
 
-	    // locales and locales display names
-	    // var _LOCALES = Object.keys(localesObj);
-	    // if (!_LOCALES || _LOCALES.length === 0) {
-	    //   console.error('There are no _LOCALES provided');
-	    // }
-	    // var _LOCALES_DISPLAY_NAMES = [];
-	    // _LOCALES.forEach(function (locale) {
-	    //   _LOCALES_DISPLAY_NAMES.push(localesObj[locale]);
-	    // });
-	    
-	    // STORING CURRENT LOCALE
+	    var localesObj = {};
 	    var currentLocale = $translate.proposedLanguage();// because of async loading
 	    
-	    // METHODS
 	    var checkLocaleIsValid = function (locale) {
 	      return Object.keys(localesObj).indexOf(locale)!== -1;
 	    };
 	    
 	    var setLocale = function (locale) {
-    	  var i;
-    	  var keys=Object.keys(localesObj);
+    	   var i;
+    	   var keys=Object.keys(localesObj);
 	      for(i=0; i<keys.length; i++){
-	      	console.log(keys[i]);
 	      	if(localesObj[keys[i]]==locale){
 	      		var localeCode=keys[i];
 	      		break;
@@ -97,14 +66,13 @@
 	      $rootScope.$broadcast('languageChange');
 	    };
 	    
-
 	    var setLocales = function(locale,localeDisplayName){
 	    	localesObj[locale]=localeDisplayName;
-	    }
-
+	    };
+	    
 	    var getLocales = function(){
 	    	return Object.keys(localesObj);
-	    }
+	    };
 
 	    // EVENTS
 	    // on successful applying translations by angular-translate
@@ -151,12 +119,8 @@
             controller: function ($scope) {
                 $scope.currentLocaleDisplayName = LocaleService.getLocaleDisplayName();
                 $scope.localesDisplayNames = LocaleService.getLocalesDisplayNames();
-
-               
-
                 $scope.visible = $scope.localesDisplayNames &&
                 $scope.localesDisplayNames.length > 1;
-    
                 $scope.changeLanguage = function (locale) {
                     LocaleService.setLocaleByDisplayName(locale);
                 };
