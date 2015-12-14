fdeb21c030177e784c5933dd3d31ea7446352738
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2015-12-14 10:40:55 -0600

    Creates JSON files with a list of countries in each locale language

--- src/main/java/com/covisint/cui/i18n/ListCountry.java
+++ src/main/java/com/covisint/cui/i18n/ListCountry.java
@@ -1,24 +1,17 @@
 package com.covisint.cui.i18n;
 
 import java.util.Locale;
-import java.util.Map;
-import java.util.TreeMap;
 import java.io.PrintWriter;
 import java.io.IOException;
 
 public class ListCountry {
 
-    private Map<String, String> languagesMap = new TreeMap<String, String>();
     public ListCountry() {
-	// initLanguageMap();
     }
 
     public static void main(String[] args) {
 	    ListCountry obj = new ListCountry();
-
-	    // obj.getListOfCountries();
         obj.listCountriesLocale();
-        //obj.countryExample(Locale.FRENCH);
     }
 
     public static void listCountriesLocale() {
@@ -26,70 +19,33 @@ public class ListCountry {
         Locale locales[] = Locale.getAvailableLocales();
         String[] isoCountries = Locale.getISOCountries();
 
-        for (String countryCode : isoCountries) {
-            for (Locale locale : locales) {
+        for (Locale locale : locales) {
 
-//            try {
-//                PrintWriter writer = new PrintWriter("./assets/json/"+localeObj.getCountry()+".json", "UTF-8");
-//                writer.println("Test");
-//                writer.close();
-//            } catch (IOException e) {
-//                System.out.println("Error writing to file: " + e);
-//            }
+            try {
+                PrintWriter writer = new PrintWriter("./assets/json/"+locale+".json", "UTF-8");
+                writer.println("[{");
+                writer.flush();
 
-                Locale localeObj = new Locale("", countryCode);
-                System.out.println(localeObj.getCountry() + ":" + localeObj.getDisplayCountry(locale));
+                for (int i = 0; i < isoCountries.length; i++) {
+                    Locale localeObj = new Locale("", isoCountries[i]);
+
+                    if (i == isoCountries.length - 1) {
+                        writer.println("\"" + localeObj.getCountry() + "\":\"" + localeObj.getDisplayCountry(locale) + "\"");
+                        writer.flush();
+                    }
+                    else {
+                        writer.println("\"" + localeObj.getCountry() + "\":\"" + localeObj.getDisplayCountry(locale) + "\",");
+                        writer.flush();
+                    }
+                }
+                writer.println("}]");
+                writer.flush();
+                writer.close();
+
+            } catch (IOException e) {
+                System.out.println("Error writing to file: " + e);
             }
-        }
-    }
-
-    public void getListOfCountries() {
-
-	    String[] countries = Locale.getISOCountries();
-
-	    int supportedLocale = 0, nonSupportedLocale = 0;
-
-	    for (String countryCode : countries) {
-
-	        Locale obj = null;
-
-	        if (languagesMap.get(countryCode) == null) {
-
-		        obj = new Locale("", countryCode);
-		        nonSupportedLocale++;
-
-	        } else {
-
-		        //create a Locale with own country's languages
-		        obj = new Locale(languagesMap.get(countryCode), countryCode);
-		        supportedLocale++;
-
-	        }
-
-            System.out.println("Country Code = " + obj.getCountry()
-		        + ", Country Name = " + obj.getDisplayCountry(obj)
-		        + ", Languages = " + obj.getDisplayLanguage());
 
         }
-
-	    System.out.println("nonSupportedLocale : " + nonSupportedLocale);
-	    System.out.println("supportedLocale : " + supportedLocale);
-
     }
-
-    // create Map with country code and languages
-    public void initLanguageMap() {
-
-	    Locale[] locales = Locale.getAvailableLocales();
-
-	    for (Locale obj : locales) {
-
-	        if ((obj.getDisplayCountry() != null) && (!"".equals(obj.getDisplayCountry()))) {
-		    languagesMap.put(obj.getCountry(), obj.getLanguage());
-	        }
-
-	    }
-
-    }
-
 }
